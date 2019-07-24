47bd92323235946dcacd89ebb82fbdb379823be5
feinubin <53144129+feinubin@users.noreply.github.com>
2019-07-24 12:54:51 +0800

    Update bbr.sh

--- bbr.sh
+++ bbr.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
-#
-# Auto install latest kernel for TCP BBR
-#
-# System Required:  CentOS 6+, Debian7+, Ubuntu12+
-#
-# Copyright (C) 2016-2018 Teddysun <i@teddysun.com>
-#
-# URL: https://teddysun.com/489.html
-#
+
 
 red='\033[0;31m'
 green='\033[0;32m'
